29b1dac051364994fc8afb0da0f2ce7a5b2244bb
sergeycrisp <sergey.crisp@gmail.com>
2021-03-04 13:41:29 +0200

    add auth controller features and update package.json with prettier

--- app_wav/models/userModel.js
+++ app_wav/models/userModel.js
@@ -15,7 +15,6 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
-  photo: String,
   role: {
     type: String,
     enum: ['user', 'guide', 'lead-guide', 'admin'],
