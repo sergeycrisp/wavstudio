3e77803803c10d56f751e2f40f578a203df719ce
sergeycrisp <sergey.crisp@gmail.com>
2021-03-18 14:29:39 +0200

    freeze upload file feature

--- app_wav/controllers/articleController.js
+++ app_wav/controllers/articleController.js
@@ -1,56 +1,51 @@
-const multer = require('multer');
-const sharp = require('sharp');
+// const multer = require('multer');
+// const sharp = require('sharp');
 const Article = require('../models/articleModel');
-const catchAsync = require('./../utils/catchAsync');
+// const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
-const AppError = require('../utils/appError');
+// const AppError = require('../utils/appError');
 
-const multerStorage = multer.memoryStorage();
+// const multerStorage = multer.memoryStorage();
 
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image')) {
-    cb(null, true);
-  } else {
-    cb(
-      new AppError('Not an image! Please upload only images.', 400),
-      false
-    );
-  }
-};
+// const multerFilter = (req, file, cb) => {
+//   if (file.mimetype.startsWith('image')) {
+//     cb(null, true);
+//   } else {
+//     cb(
+//       new AppError('Not an image! Please upload only images.', 400),
+//       false
+//     );
+//   }
+// };
 
-const upload = multer({
-  storage: multerStorage,
-  fileFilter: multerFilter,
-});
+// const upload = multer({
+//   storage: multerStorage,
+//   fileFilter: multerFilter,
+// });
 
-exports.uploadArticleImages = upload.fields([
-  { name: 'imageCover', maxCount: 1 },
-  { name: 'images', maxCount: 10 },
-]);
+// exports.uploadArticleImages = upload.fields([
+//   { name: 'imageCover', maxCount: 1 },
+// ]);
 
-// upload.single('image') req.file
-// upload.array('images', 5) req.files
+// exports.resizeArticleImages = catchAsync(async (req, res, next) => {
+//   console.log(req.files.imageCover);
+//   if (!req.files.imageCover) return next();
+//   // 1) Cover image
+//   req.body.imageCover = `articles-${
+//     req.params.id
+//   }-${Date.now()}-cover.jpg`;
+//   await sharp(req.files.imageCover[0].buffer)
+//     .resize(2000, 1333)
+//     .toFormat('jpg')
+//     .jpeg({ quality: 90 })
+//     .toFile(`public/img/articles/${req.body.imageCover}`);
 
-exports.resizeArticleImages = catchAsync(async (req, res, next) => {
-  console.log(req.files.imageCover);
-  if (!req.files.imageCover) return next();
-  // 1) Cover image
-  req.body.imageCover = `articles-${
-    req.params.id
-  }-${Date.now()}-cover.jpg`;
-  await sharp(req.files.imageCover[0].buffer)
-    .resize(2000, 1333)
-    .toFormat('jpg')
-    .jpeg({ quality: 90 })
-    .toFile(`public/img/articles/${req.body.imageCover}`);
-
-  next();
-});
+//   next();
+// });
 
 exports.aliasTopArticles = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = 'likes';
-  // req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
 
