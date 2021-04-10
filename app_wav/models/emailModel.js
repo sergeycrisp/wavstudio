da0cf15ecbabdaf1b8ce6fbd1c9f177b37c6add2
sergeycrisp <sergey.crisp@gmail.com>
2021-04-10 14:03:19 +0300

    add email subscribe feature

--- app_wav/models/emailModel.js
+++ app_wav/models/emailModel.js
@@ -4,6 +4,8 @@ const emailSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'There is email?'],
+    minLength: [4, 'A email must be longer, than 4 symbols'],
+    maxLength: [100, 'A email must be shorter, than 100 symbols'],
   },
 });
 
